c558afc99d5ff1ba2f5030bfa5d73232ebcc900a
sanchit <sanchitgarg.1909@gmail.com>
2019-06-29 17:27:09 +0530

    Initial Commit

---
+++ IxigoDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IxigoDemo
+//
+//  Created by Sanchit Garg on 29/06/19.
+//  Copyright © 2019 Sanchit Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
